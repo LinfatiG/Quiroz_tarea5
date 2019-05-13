2b9560345b54c0ffafed2330df86f39670f716cb
alvaro021 <43354155+alvaro021@users.noreply.github.com>
2019-05-12 20:49:08 -0400

    Update InteraccionUsuario.java

--- InteraccionUsuario.java
+++ InteraccionUsuario.java
@@ -1,58 +1,50 @@
 package model.dominioProblema;
-
-
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
+import java.util.*;
 
 public class InteraccionUsuario
-{
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
-	public InteraccionUsuario(){
-		super();
+{	
+    Repertorio rp = new Repertorio();
+	public void menu() {
+            int op = 0;
+            do{
+		System.out.println("ingrese opcion");
+                System.out.println("1- ver cancion");
+                System.out.println("2- eliminar cancion");
+                System.out.println("3- agregar cancion");
+                System.out.println("4- salir");
+                Scanner sc = new Scanner(System.in);
+                try{
+                op = sc.nextInt();
+                llamadaMetodos(op);
+                }catch(Exception e){
+                System.out.println("opcion mal ingresada");
+                }
+            } while(op != 4);
 	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
 	
-	public Void menu() {
-		// TODO implement me
-		return null;
+	public void llamadaMetodos( int op) {
+		
+            switch(op){
+                case 1:
+                    rp.mostrarCancion();
+                    break;
+                case 2:
+                    rp.quitarCancion();
+                    break;
+                case 3:
+                    rp.anadirCancion();
+                    break;
+                case 4:
+                    salir();
+                default:
+                    System.out.println("opcion mal ingresada");
+                    break;
+            }
+            
 	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
 	
-	public Void llamadaMetodos() {
-		// TODO implement me
-		return null;
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public Void salir() {
-		// TODO implement me
-		return null;
+	public void salir() {
+		System.exit(10);
 	}
 
 }
-
