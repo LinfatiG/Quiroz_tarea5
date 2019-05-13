d6c277921082081b8744d56f89862916fccc4ec7
alvaro021 <43354155+alvaro021@users.noreply.github.com>
2019-05-12 20:48:47 -0400

    Update Repertorio.java

--- Repertorio.java
+++ Repertorio.java
@@ -1,81 +1,62 @@
 package model.dominioProblema;
-import java.util.HashSet;
-import java.util.Set;
-
-
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
+import java.util.*;
+import java.io.*;
+import java.nio.*;
+import model.datos.Archivo;
 
 public class Repertorio
 {
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
 	
-	public Set<Cancion> libros;
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
-	public Repertorio(){
-		super();
+	
+	public ArrayList<Cancion> canciones;
+	
+	public void anadirCancion() {
+		try{
+                System.out.println("ingrese nombre de cancion");
+                Scanner sc = new Scanner(System.in);
+                String nombre = sc.nextLine();
+                System.out.println("ingrese albun");
+                String albun = sc.nextLine();
+                System.out.println("ano lanzamiento");
+                int anoLanzamiento = sc.nextInt();
+                System.out.println("interprete principal");
+                String interPrincipal = sc.nextLine();
+                String letra = ingresarLetra();
+                int codigo = generarCodigo();
+                Cancion cancion = new Cancion(nombre,albun,anoLanzamiento,letra, interPrincipal, codigo);
+                canciones.add(cancion);
+                } catch(Exception e){
+                System.out.println("no se ha podido agregar la cancion");
+                }
 	}
 
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
+	public void quitarCancion() {
+            try{
+            Scanner sc = new Scanner(System.in);
+            System.out.println("ingrese el numero de la cancion que desea eliminar");
+            int eliminar = sc.nextInt();
+            canciones.remove(eliminar);
+            }catch(Exception e){
+            System.out.println("no se ha podido eliminar la cancion");
+            }
+        }
 	
-	public Void anadirCancion() {
-		// TODO implement me
-		return null;
+	public void mostrarCancion() {
+            try{
+            Archivo ar = new Archivo();
+            ar.leerArchivo();
+            }catch(Exception e){
+            System.out.println("no se ha podido mostrar la cancion");
+            }
 	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public Void quitarCancion() {
-		// TODO implement me
-		return null;
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public Void mostrarCancion() {
-		// TODO implement me
-		return null;
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
 	
 	public int generarCodigo() {
-		// TODO implement me
-		return 0;
+		int codigo = canciones.size()+1;
+		return codigo;
 	}
 
+        public String ingresarLetra(){
+            Archivo ar = new Archivo();
+            return ar.agregarTexto();
+        }
 }
-
